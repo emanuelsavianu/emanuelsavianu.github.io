a4e210aa0aeafcb536e326fb2edb360976d473ef
emanuelsavianu <emanuelsavianu@gmail.com>
2026-04-03 12:12:59 +0200

    fix phone v2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Dr. Savianu Medical Website
 // Cache version bumped to v75 for metadata/accessibility refresh
-const CACHE_NAME = 'savianu-v253';
+const CACHE_NAME = 'savianu-v257';
 const urlsToCache = [
   '/',
   '/index.html',
