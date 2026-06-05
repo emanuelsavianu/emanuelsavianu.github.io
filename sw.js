ea3e9a24c162be4b4d4f2b33510e44cb22d0c240
emanuelsavianu <emanuelsavianu@gmail.com>
2026-06-05 22:30:32 +0200

    fusi orari v2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Dr. Savianu Medical Website
 // Cache version bumped to v75 for metadata/accessibility refresh
-const CACHE_NAME = 'savianu-v304';
+const CACHE_NAME = 'savianu-v305';
 const urlsToCache = [
   '/',
   '/index.html',
